3fff8fa7fcce5897bb05de2d938aefd57251fcae
RAOUL FOTSARA <rfosso@nexcqisolutions.com>
2026-04-24 09:47:32 +0100

    fix: replace bcrypt native with bcryptjs for Render compatibility

--- src/database/simple-sqlite.js
+++ src/database/simple-sqlite.js
@@ -5,7 +5,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const fs = require('fs');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 class SimpleSQLite {
   constructor(databasePath) {
